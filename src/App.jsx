560304ea8580ea3228834aee5a5f2e9731e0071e
YJ Yang <chcokr@gmail.com>
2015-08-08 19:48:03 -0400

    Scoreboard templating

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,49 @@ require('./App.less');
 
 require('babel/polyfill');
 
+import lessVars from '!!less-interop!./App.less'
+import ScoreBoard from './components/ScoreBoard'
+
+import BSNav from 'react-bootstrap/lib/Nav';
+import BSNavbar from 'react-bootstrap/lib/Navbar';
+import BSNavItem from 'react-bootstrap/lib/NavItem';
 import React from 'react';
+import DocumentTitle from 'react-document-title';
+import {Route, Router} from 'react-router';
+import {history} from 'react-router/lib/HashHistory';
+import style from 'stilr-classnames';
+import StylesheetHotLoad from 'chcokr-webpack/StylesheetHotLoad';
+
+export class AppTemplate extends React.Component {
+  render() {
+    return (
+      <DocumentTitle title="Dufence">
+        <StylesheetHotLoad>
+          <div>
+            <BSNavbar>
+              <BSNav navbar right>
+                <BSNavItem>+ New game</BSNavItem>
+                <BSNavItem>
+                  <span {...style({color: lessVars.brandPrimary})}>
+                    See all games
+                  </span>
+                </BSNavItem>
+              </BSNav>
+            </BSNavbar>
+            <ScoreBoard />
+          </div>
+        </StylesheetHotLoad>
+      </DocumentTitle>
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-      </div>
+      <Router history={history}>
+        <Route path="/" component={AppTemplate} />
+      </Router>
     );
   }
 }
