fa224771e9b9c1f2784be02d19a30cc61d946161
YJ Yang <chcokr@gmail.com>
2015-08-09 03:20:57 -0400

    Change initial mock data

--- src/appStateTree.jsx
+++ src/appStateTree.jsx
@@ -4,9 +4,9 @@ const Baobab = require('baobab');
 
 const appStateTree = new Baobab({
   games: normalize([
-    getNewGame('Cleveland St.', new Date(2015, 7, 20)),
-    getNewGame('Notre Dame', new Date(2015, 7, 20)),
-    getNewGame('UNC', new Date(2015, 7, 21))
+    getNewGame('Cleveland St.', new Date(2015, 6, 20)),
+    getNewGame('Notre Dame', new Date(2015, 6, 20)),
+    getNewGame('UNC', new Date(2015, 6, 21))
   ])
 });
 export default appStateTree;
